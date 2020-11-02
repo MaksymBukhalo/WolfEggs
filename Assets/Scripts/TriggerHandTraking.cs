76ed862bf6be0b2acfc1e2ccaedf47e223d3a860
Maksim Bukhalo <maksimbukhalo1314@gmail.com>
2020-11-02 18:12:23 +0200

    feat() Add HandsTraking

---
+++ Assets/Scripts/TriggerHandTraking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHandTraking : MonoBehaviour
+{
+	public GameObject Cap;
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.layer == 16 || other.gameObject.layer == 17)
+		{
+			Cap.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.layer == 16 || other.gameObject.layer == 17)
+		{
+			Cap.SetActive(false);
+		}
+	}
+}
